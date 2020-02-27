79717171a254b8aab28cb4b89c0ce58a69ebb518
pcmxgti <16561338+pcmxgti@users.noreply.github.com>
2020-02-27 08:43:38 -0500

    different base containers

--- build_container.sh
+++ build_container.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -eu -o pipefail
-export VERSION=$(awk -F\' '/__version__/{print $2}' ./tokendito/__version__.py)
-docker build --build-arg version="${VERSION}" -t tokendito .
+export VERSION=${1:-$(awk -F\' '/__version__/{print $2}' ./tokendito/__version__.py)}
+
+for variant in Dockerfile-*; do
+    docker build --file "${variant}" --build-arg version="${VERSION}" -t tokendito-"${variant/Dockerfile-/}" .
+done
